58ca7678a410a69de95eedfad33c212fb34d6df7
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2022-11-11 19:20:44 +0000

    init demo flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "An example NixOS configuration for testing the nix-dabei kexec auto-installer";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+
+      mysystem = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        networking.hostName = "mysystem";
+
+        boot.loader.systemd-boot.enable = true;
+
+        services.sshd.enable = true;
+        users.users.root.openssh.authorizedKeys.keys = [ "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBPKBVrtVHb2Y8L2ntrjF8R6zF06WF+fP49YcJglgXLPwqSWwCE+osLz/ylDPTcufznhz72qFswB25r74HSGUSLwAAAAEc3NoOg== HTZ__nix-dabei__dante" ];
+
+        imports = [
+          "${modulesPath}/installer/scan/not-detected.nix"
+        ];
+
+        specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}

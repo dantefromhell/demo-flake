6a88f6093419955455b40a2f477c52fbed90e423
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2022-11-11 21:55:28 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,14 +9,14 @@
       mysystem = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         
-        config = {
+        modules = [ ({ ... }: {
           networking.hostName = "mysystem";
 
           boot.loader.systemd-boot.enable = true;
 
           services.sshd.enable = true;
           users.users.root.openssh.authorizedKeys.keys = [ "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBPKBVrtVHb2Y8L2ntrjF8R6zF06WF+fP49YcJglgXLPwqSWwCE+osLz/ylDPTcufznhz72qFswB25r74HSGUSLwAAAAEc3NoOg== HTZ__nix-dabei__dante" ];
-        };
+        }) ];
       };
     };
   };

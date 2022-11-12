f13c3ac28844a16a1dbea149ad0107f0b3bb4458
dantefromhell <dantefromhell@users.noreply.github.com>
2022-11-12 00:49:10 +0000

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -34,8 +34,7 @@
 
           services.sshd.enable = true;
           users.users.root.openssh.authorizedKeys.keys = [ "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBPKBVrtVHb2Y8L2ntrjF8R6zF06WF+fP49YcJglgXLPwqSWwCE+osLz/ylDPTcufznhz72qFswB25r74HSGUSLwAAAAEc3NoOg== HTZ__nix-dabei__dante" ];
-})
-        ];
+        } ) ];
       };
     };
   };

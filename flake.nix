f4c044ea8b39751309d3426dc894e18d82d6ae52
dantefromhell <dantefromhell@users.noreply.github.com>
2022-11-13 17:14:25 +0000

    Add system demo and custom nix-dabei configuration into a single flake.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,51 @@
+#
+# Based on gist https://gist.github.com/phaer/e0a5cd7458cb13b70b43d50659b94c17
+# provided by phaer.
+#
+# This flake builds the 'kexec' branch of nix-dabei with ability to do custom
+# configuration.
+
 {
   description = "An example NixOS configuration for testing the nix-dabei kexec auto-installer";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-dabei.url = "github:dep-sys/nix-dabei/kexec";
+  };
 
-  outputs = { self,  nixpkgs }: {
+  outputs = { self, nix-dabei, nixpkgs }: let
+    sshKeys = [ "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBPKBVrtVHb2Y8L2ntrjF8R6zF06WF+fP49YcJglgXLPwqSWwCE+osLz/ylDPTcufznhz72qFswB25r74HSGUSLwAAAAEc3NoOg== HTZ__nix-dabei__dante" ];
+    bootDisk = "/dev/sda";
+  in {
+    # Build 'nix-dabei'
+    kexec = (nix-dabei.lib.makeInstaller [
+        ({ config, modulesPath, ... }: {
+          imports = [
+            "${modulesPath}/profiles/qemu-guest.nix"
+          ];
+          config = {
+            nix-dabei = {
+              zfs.enable = true;
+              diskDevice = bootDisk;
+            };
+            users.users.root.openssh.authorizedKeys.keys = sshKeys;
+          };
+        })
+      ] ).config.system.build.kexec;
+
+
+    # Systems to configure/ deploy.
     nixosConfigurations = {
-      nix-dabei = nixpkgs.lib.nixosSystem {
+      nix-dabei-demo = ( nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ({ ... }: {
-          networking.hostName = "nix-dabei";
-          networking.hostId = builtins.substring 0 8 (builtins.hashString "sha256" "nix-dabei");
+          networking.hostName = "nix-dabei-demo";
+          networking.hostId = builtins.substring 0 8 (builtins.hashString "sha256" "nix-dabei-demo");
           system.stateVersion = "22.11";
 
           boot.loader.grub = {
             enable = true;
-            devices = [ "/dev/sda" ];
+            devices = [ bootDisk ];
           };
           fileSystems = {
             "/" = {
@@ -36,9 +67,9 @@
           };
 
           services.sshd.enable = true;
-          users.users.root.openssh.authorizedKeys.keys = [ "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBPKBVrtVHb2Y8L2ntrjF8R6zF06WF+fP49YcJglgXLPwqSWwCE+osLz/ylDPTcufznhz72qFswB25r74HSGUSLwAAAAEc3NoOg== HTZ__nix-dabei__dante" ];
+          users.users.root.openssh.authorizedKeys.keys = sshKeys;
         } ) ];
-      };
+      } ).config.system.build.toplevel;
     };
   };
 }
